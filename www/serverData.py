bddbec4730aa9de48aaeec60e9e6cd54004c28c7
lamter <lamter.fu@gmail.com>
2015-05-25 15:11:48 +0800

    - add : 完成结构框架

---
+++ www/serverData.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+'''
+Created on 2015-05-23
+
+@author: Shawn
+'''
+
+__author__ = 'lamter'
+
+
+
+class ServerData(object):
+    """
+    全局的数据容器
+    """
+    def __init__(self):
+        """
+
+        :return:
+        """
+
+
